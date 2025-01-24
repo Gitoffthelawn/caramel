8f39f9f0fabdc24523a1921c290fecab8da4c9a3
MedAmine1212 <khaili.amine@hotmail.fr>
2025-01-24 12:32:58 +0100

    fix

--- caramel-landing/src/layouts/layout/Layout.jsx
+++ caramel-landing/src/layouts/layout/Layout.jsx
@@ -6,7 +6,7 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 
-export default function layout({ children }) {
+export default function Layout({ children }) {
     const { isDarkMode } = useContext(ThemeContext)
     const ref = useRef(null)
   return (
