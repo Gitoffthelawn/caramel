b28070ca4452e0f5d30df204f193586b04cc9617
MedAmine1212 <khaili.amine@hotmail.fr>
2025-01-31 23:47:33 +0100

    more logs..

--- caramel-extension/inject.js
+++ caramel-extension/inject.js
@@ -11,14 +11,13 @@ window.addEventListener("load", async () => {
     const domainRecord = await getDomainRecord(domain);
     console.log("Caramel: Domain record", domainRecord)
     if (domainRecord) {
-        const fullUrl = window.location.href;
-        if (fullUrl) {
-            const input = await document.querySelector(`${domainRecord.couponInput}`);
-            const showInputButton = await document.querySelector(`${domainRecord.showInput}`);
-            if (input || showInputButton) {
-                console.log("Caramel: Detected checkout page");
-                initCouponFlow(domainRecord);
-            }
+        const input = await document.querySelector(`${domainRecord.couponInput}`);
+        console.log("Caramel: Input", input);
+        const showInputButton = await document.querySelector(`${domainRecord.showInput}`);
+        console.log("Caramel: Show input button", showInputButton);
+        if (input || showInputButton) {
+            console.log("Caramel: Detected checkout page");
+            initCouponFlow(domainRecord);
         }
     }
 });
