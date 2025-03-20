f42b0d041ec9f470759fbdf576c1a73bca866682
MedAmine1212 <khaili.amine@hotmail.fr>
2025-03-20 04:17:23 +0100

    remove ts

--- caramel-landing/src/pages/api/extension/login.js
+++ caramel-landing/src/pages/api/extension/login.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import prisma from "@/lib/prisma";
+import prisma from "@prisma/client";
 
 export default async function handler(req, res) {
     if (req.method !== "POST") {
