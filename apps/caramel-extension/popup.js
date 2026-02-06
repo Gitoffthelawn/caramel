29482d5ff5efd440dbfae865e8c0d3618c589a7e
kd_kinuthiadavid <kd.kinuthiadavid@gmail.com>
2026-02-06 16:15:06 +0300

    feat(extension): add OAuth sign-in buttons to login prompt
    - Add Google and Apple OAuth buttons with inline SVG icons
    - Add 'or' divider between OAuth and email/password login
    - Style buttons to match extension design (caramel orange theme)
    - Position buttons above email/password form

--- apps/caramel-extension/popup.js
+++ apps/caramel-extension/popup.js
@@ -126,6 +126,28 @@ function renderSignInPrompt(backFn) {
     container.innerHTML = `
     <div class="login-prompt fade-in-up">
 
+      <div class="oauth-buttons">
+        <button type="button" id="googleSignInBtn" class="oauth-button" disabled>
+          <svg class="oauth-icon" width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#4285F4" d="M17.64 9.2c0-.637-.057-1.251-.164-1.84H9v3.481h4.844c-.209 1.125-.843 2.078-1.796 2.717v2.258h2.908c1.702-1.567 2.684-3.874 2.684-6.615z"/>
+            <path fill="#34A853" d="M9 18c2.43 0 4.467-.806 5.956-2.184l-2.908-2.258c-.806.54-1.837.86-3.048.86-2.344 0-4.328-1.584-5.036-3.711H.957v2.332C2.438 15.983 5.482 18 9 18z"/>
+            <path fill="#FBBC05" d="M3.964 10.707c-.18-.54-.282-1.117-.282-1.707s.102-1.167.282-1.707V4.961H.957C.348 6.175 0 7.55 0 9s.348 2.825.957 4.039l3.007-2.332z"/>
+            <path fill="#EA4335" d="M9 3.58c1.321 0 2.508.454 3.44 1.345l2.582-2.58C13.463.891 11.426 0 9 0 5.482 0 2.438 2.017.957 4.961L3.964 7.293C4.672 5.163 6.656 3.58 9 3.58z"/>
+          </svg>
+          <span>Sign in with Google</span>
+        </button>
+        <button type="button" id="appleSignInBtn" class="oauth-button" disabled>
+          <svg class="oauth-icon" width="18" height="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#000000" d="M17.05 20.28c-.98.95-2.05.88-3.08.4-1.09-.5-2.08-.48-3.24 0-1.44.62-2.2.44-3.06-.4C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09l.01-.01zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z"/>
+          </svg>
+          <span>Sign in with Apple</span>
+        </button>
+      </div>
+
+      <div class="oauth-divider">
+        <span>or</span>
+      </div>
+
       <form id="loginForm" class="login-form">
         <div id="loginErrorMessage" class="error-message" style="display:none;"></div>
 
